21537b75a1b43a960f4aec1378f16b7efc20b869
Unknown <jeorch@foxmail.com>
2019-11-06 19:02:27 +0800

    aggration 一层成功

--- modules/es-sql/model/es-sql-reponse.go
+++ modules/es-sql/model/es-sql-reponse.go
@@ -1,5 +1,7 @@
 package model
 
+import "strings"
+
 type EsSQLResponse struct {
 	Took 			int64		`json:"took"`
 	TimeOut 		bool		`json:"time_out"`
@@ -48,11 +50,61 @@ func (esr EsSQLResponse) FormatSource() [][]interface{} {
 		return nil
 	}
 
-	return dealAggregations(esr.Aggregations)
-}
-
-func dealAggregations(agg interface{}) [][]interface{} {
-	//TODO:
+	return dealAggregations(esr.Aggregations.(map[string]interface{}))
 	return nil
 }
+
+func dealAggregations(data map[string]interface{}) [][]interface{} {
+	//TODO:
+	resultMap := dealAboveBuckets(data)
+	result := make([][]interface{},0)
+
+	for k , v := range resultMap {
+		tmp := make([]interface{}, 0 )
+		key := strings.ReplaceAll(k, ".keyword", "")
+		tmp = append(tmp, key)
+		tmp = append(tmp, v[:]...)
+		result = append(result, tmp)
+	}
+
+	return result
+}
+
+func dealAboveBuckets(data map[string]interface{}) map[string][]interface{} {
+
+	result := make(map[string][]interface{})
+
+	for bucketsKey, bucketsValue := range data {
+		valueMap := bucketsValue.(map[string]interface{})
+		if buckets, ok := valueMap["buckets"]; ok {
+			for _, item := range buckets.([]interface{}) {
+				bucket := item.(map[string]interface{})
+				key := bucket["key"]
+				delete(bucket, "key")
+				delete(bucket, "doc_count")
+
+				sub := dealAboveBuckets(bucket)
+				//TODO: check one map
+				if len(sub) > 1 {
+					for k, _ := range bucket {
+						delete(sub, k)
+					}
+				}
+
+				for subK, subV := range sub {
+					for range subV {
+						result[bucketsKey] = append(result[bucketsKey], key)
+					}
+					result[subK] = append(result[subK], subV[:]...)
+				}
+			}
+		} else {
+			result[bucketsKey] = append(result[bucketsKey], valueMap["value"])
+		}
+	}
+
+	return result
+}
+
+
 
